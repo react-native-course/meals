bc3443c857b30638f5485ab5531e4c6cae9d95ad
Adam Morsi <adam.morsi@yahoo.com>
2019-11-02 21:09:54 +0200

    add content in meal detail screen

--- Screens/MealDetailScreen.js
+++ Screens/MealDetailScreen.js
@@ -1,24 +1,61 @@
 import React from 'react';
-import {StyleSheet, Text, View} from "react-native";
+import {StyleSheet, Text, View, ScrollView, Image} from "react-native";
 import {HeaderButtons, Item} from 'react-navigation-header-buttons';
 import {MEALS} from "../data/dummy-data";
 import HeaderButton from '../Components/HeaderButton';
+import DefaultText from "../Components/DefaultText";
 
 const styles = StyleSheet.create({
-    screen: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center'
+    image: {
+        width: '100%',
+        height: 200,
+    },
+    details: {
+        flexDirection: 'row',
+        justifyContent: 'space-around',
+        padding: 15
+    },
+    title: {
+        fontFamily: 'open-sans-bold',
+        fontSize: 22,
+        textAlign: 'center'
+    },
+    listItem:  {
+        marginVertical: 10,
+        marginHorizontal: 20,
+        borderColor: "#ccc",
+        borderWidth: 1,
+        padding: 10,
     }
 });
+
+const ListItem = ({children}) => (
+  <View style={styles.listItem}>
+      <DefaultText>{children}</DefaultText>
+  </View>
+);
+
 const MealDetailScreen = ({navigation: {getParam}}) => {
     const mealId = getParam('mealId'),
         selectedMeal = MEALS.find(meal => meal.id === mealId);
 
     return (
-        <View style={styles.screen}>
-            <Text>{selectedMeal.title}</Text>
-        </View>
+        <ScrollView>
+            <Image source={{uri: selectedMeal.imageUrl}} style={styles.image}/>
+            <View style={styles.details}>
+                <DefaultText>{selectedMeal.duration}</DefaultText>
+                <DefaultText>{selectedMeal.complexity.toUpperCase()}</DefaultText>
+                <DefaultText>{selectedMeal.affordability.toUpperCase()}</DefaultText>
+            </View>
+            <Text style={styles.title}>Ingredients</Text>
+            {selectedMeal.ingredients.map((ingredient) => (
+                <ListItem key={ingredient}>{ingredient}</ListItem>
+            ))}
+            <Text style={styles.title}>Steps</Text>
+            {selectedMeal.steps.map((step) => (
+                <ListItem key={step}>{step}</ListItem>
+            ))}
+        </ScrollView>
     );
 };
 
